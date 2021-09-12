3b1b085ddf88ef26d222d4c746d0c5769f6851f1
filices <47802256+filices@users.noreply.github.com>
2021-09-12 19:35:01 -0400

    Update literals.py
    Changed default logging handler to prevent browser errors from being logged to console

--- mayan/apps/logging/literals.py
+++ mayan/apps/logging/literals.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 DEFAULT_ERROR_LOG_PARTITION_ENTRY_LIMIT = 3
 DEFAULT_LOGGING_ENABLE = True
-DEFAULT_LOGGING_HANDLERS = ('console',)
+DEFAULT_LOGGING_HANDLERS = ('logfile',)
 DEFAULT_LOGGING_LEVEL = 'ERROR'
 DEFAULT_LOGGING_LOG_FILE_PATH = os.path.join(settings.MEDIA_ROOT, 'error.log')
 
